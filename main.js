d0ee8d7ba11ecb91075564056962358aa6766d82
JuanchoTopadora <juanmanuellong@gmail.com>
2024-06-29 20:54:45 -0300

    TERMINADO

--- main.js
+++ main.js
@@ -1,34 +1,16 @@
-import HelloWorldScene from "./scenes/HelloWorldScene.js";
+import Game from './scenes/Game.js';
 
-// Create a new Phaser config object
 const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    min: {
-      width: 800,
-      height: 600,
+    type: Phaser.AUTO,
+    width: 1200,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
     },
-    max: {
-      width: 1600,
-      height: 1200,
-    },
-  },
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 200 },
-      debug: true,
-    },
-  },
-  // List of scenes to load
-  // Only the first scene will be shown
-  // Remember to import the scene before adding it to the list
-  scene: [HelloWorldScene],
+    scene: [Game]
 };
 
-// Create a new Phaser game instance
-window.game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
